6a5656465ed637b7e163f828622317b4f7b1d78e
gowtham659 <gowthamthota121@gmail.com>
2024-04-27 22:16:20 +0530

    gohttpgetreg updated

--- webprog/GoHttpGetReq.go
+++ webprog/GoHttpGetReq.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
+type httpresp struct{
+	Args arg        `json:"args"`
+	Headers header  `json:"headers"`
+	Org string		`json:"origin"`
+	Url string		`json:"url"`
+}
+type header struct{
+	Aenc string    `json:"Accept-Encoding"`
+	Host string		`json:"Host"`
+	Uagent string	`json:"User-Agent"`
+	Tid string		`json:"X-Amzn-Trace-Id"`
+}
+type arg struct{
+
+}
 func main(){
 	var res *http.Response
 	var err error
@@ -16,6 +32,14 @@ func main(){
 	body,err := ioutil.ReadAll(res.Body)
 	fmt.Println(string(body))
 
+	var hres httpresp
+	err1 := json.Unmarshal(body,&hres)
+	if err1 != nil {
+		fmt.Println(err1)
+	}	
 
-
+	fmt.Println(hres.Args)
+	fmt.Println(hres.Headers.Aenc,hres.Headers.Host,hres.Headers.Tid,hres.Headers.Uagent)
+	fmt.Println(hres.Org)
+	fmt.Println(hres.Url)
 }
